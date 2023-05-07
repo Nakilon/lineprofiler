1000e44e853f106d2e2a978ba674f2d758d3e355
Victor Maslov <nakilon@gmail.com>
2023-05-07 08:40:57 +0300

    some reporting speed improvement

--- lib/lineprofiler.rb
+++ lib/lineprofiler.rb
@@ -7,12 +7,16 @@ Thread.new do
 end
 END {
   require "io/console"
-  ([$0, *ENV["LINEPROFILER"]] - %w{-e}).each do |filename|
+  ([File.expand_path($0), *ENV["LINEPROFILER"]] - %w{-e}).each do |filename|
     puts filename
-    File.foreach(filename).with_index do |line, i|
-      v = profile.count do |bt|
-        bt.include? [File.expand_path(filename), i + 1]
-      end.fdiv(profile.size).round(4) * 100
+    report = File.foreach(filename).map{ |line| [line, 0] }
+    profile.each do |bt|
+      bt.each do |path, lineno|
+        report[lineno - 1][1] += 1 if File.expand_path(filename) == path
+      end
+    end
+    report.each do |line, v|
+      v = v.fdiv(profile.size).round(4) * 100
       puts "%-8s%s" % [("%5.2f%%" % v unless v.zero?).to_s, line[0, IO.console.winsize[1]-10]]
     end
     puts ""
